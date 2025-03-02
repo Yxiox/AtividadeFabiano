e0ecf22ac79416614e2c32630958454c3f04495d
Yxiox <bruno.dela.goncalves@gmail.com>
2025-03-02 19:10:54 -0300

    Atividade2Finished

--- Atividade2/app/src/main/java/com/example/atividade2/MainActivity.kt
+++ Atividade2/app/src/main/java/com/example/atividade2/MainActivity.kt
@@ -4,11 +4,17 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.FlowRowScopeInstance.weight
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardColors
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -16,6 +22,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.atividade2.ui.theme.Atividade2Theme
 
 class MainActivity : ComponentActivity() {
@@ -26,7 +33,7 @@ class MainActivity : ComponentActivity() {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                    color = Color.Black
                 ) {
                     Screen()
                 }
@@ -37,10 +44,18 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Screen() {
-    Column () {
+    Row (modifier = Modifier.fillMaxSize()) {
+        Column (modifier = Modifier.fillMaxSize().weight(1f)) {
+            Box(modifier = Modifier.fillMaxSize().weight(1f).padding(5.dp).background(Color.DarkGray)) {  }
+            Box(modifier = Modifier.fillMaxSize().weight(1f).padding(5.dp).background(Color.Magenta)) {  }
 
-    }
-    Column () {
+        }
+        Column (modifier = Modifier.fillMaxSize().weight(1.5f)) {
+            Box(modifier = Modifier.fillMaxSize().weight(1f).padding(5.dp).background(Color.Red)) {}
+            Box(modifier = Modifier.fillMaxSize().weight(1f).padding(5.dp).background(Color.White)) {  }
+            Box(modifier = Modifier.fillMaxSize().weight(1f).padding(5.dp).background(Color.Blue)) {  }
+
+        }
 
     }
 }
